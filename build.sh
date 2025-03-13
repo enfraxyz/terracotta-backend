313fea43d8df34dcea850c54536a0591f5ada34e
Casey Wilcox <caseywilcox@outlook.com>
2025-03-13 10:14:21 -0700

    updating build file

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-# Install Terraform
-wget https://releases.hashicorp.com/terraform/1.6.0/terraform_1.6.0_linux_amd64.zip
-unzip terraform_1.6.0_linux_amd64.zip
-chmod +x terraform
-mv terraform /usr/local/bin/
+set -e  # Exit if any command fails
 
-echo "Installing dependencies..."
-npm install
+# Define Terraform version
+TERRAFORM_VERSION="1.11.2"  # Change this if needed
+
+# Create a bin directory in home if not exists
+mkdir -p "$HOME/bin"
+
+# Download Terraform binary
+curl -fsSL -o "$HOME/bin/terraform.zip" "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+
+# Unzip Terraform binary
+cd "$HOME/bin"
+unzip terraform.zip
+rm terraform.zip
+
+# Make Terraform executable
+chmod +x "$HOME/bin/terraform"
+
+# Add Terraform to PATH for the current session
+export PATH="$HOME/bin:$PATH"
+
+# Verify installation
+terraform --version
